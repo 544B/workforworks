dcf71651f6e678fc1cc9f4a4363cd90229c00922
ktakei <ktakei@geek.co.jp>
2014-03-04 19:59:11 +0900

    change setFont

--- refonter.jsx
+++ refonter.jsx
@@ -1,18 +1,20 @@
-fonty()
-function fonty(){
-  setFont = "KozGoPro-Regular";
-  reFont =  "HiraKakuPro-W3";
-  regObj = new RegExp(setFont,"g");
-  layObj = activeDocument.artLayers;
-  for (i=0; i<layObj.length; i++) {
-    if (layObj[i].kind === LayerKind.TEXT) {
-      fontName = layObj[i].textItem.font;
-      result = fontName.match(regObj);
-      if (result) {
-        fontSize = eval(layObj[i].textItem.size) * 2;
-        layObj[i].textItem.size = fontSize;
-        layObj[i].textItem.font = reFont;
-      }
-    }
-  }
+refont()
+function refont(){
+	setFont = "MS-PGothic";
+	reFont =  "HiraKakuPro-W3";
+	fontSize = 24;
+	regObj = new RegExp(setFont,"g");
+	layObj = activeDocument.layers;
+	for (i=0; i<layObj.length; i++) {
+		current = layObj[i];
+		if (current.kind === LayerKind.TEXT) {
+			fontName = current.textItem.font;
+			result = fontName.match(regObj);
+			if (result) {
+				// fontSize = eval(layObj[i].textItem.size) * 2;
+				current.textItem.size = fontSize;
+				current.textItem.font = reFont;
+			}
+		}
+	}
 }
