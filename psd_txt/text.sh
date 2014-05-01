d7245bc03bf61dbb323cc58722e6b22a5666e9fa
lpcwww <lpc.www.com@gmail.com>
2014-05-02 06:55:38 +0900

    Add Psd Text

---
+++ psd_txt/text.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+ruby text.rb \
+| perl -p -e 's/(\r\n|\n|\r)/\r\n===================================================================\r\n/g' \
+| perl -p -e 's/\\n/\r\n/g' \
+| perl -p -e 's/^(\r\n|\n|\r)//g' \
+>>text.txt
